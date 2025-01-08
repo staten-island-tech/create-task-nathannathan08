205d7870f00399e30e7bc2d964ef46c464e5ab4a
nathannathan08 <nathanc245@nycstudents.net>
2025-01-08 14:00:23 -0500

    ir6

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -3,25 +3,30 @@ import '../css/style.css';
 let balance = 2000;          
 let wins = 0;
 let losses = 0;
+let gameHistory = [];
 function flipCoin(guess) {
-    
     let betAmount = parseFloat(document.getElementById("betAmount").value);
     let resultMessage = "";
-
     if (isNaN(betAmount) || betAmount <= 0) {
         alert("You can't bet zero dollars.");
         return;
     }       
-    
   document.getElementById("result").innerHTML = "";
 
     let flipResult = Math.random() < 0.5 ? 'heads' : 'tails';  
-    
+    let gameOutcome = {
+        guess: guess,
+        flipResult: flipResult,
+        betAmount: betAmount,
+        win: false 
+    };
+
     if (guess === flipResult) {
         resultMessage = `You guessed ${guess}, and the coin landed on ${flipResult}! You can't stop winning!!!! You won $${betAmount}!`;
         document.getElementById("result").classList.add('win-message');    
         balance += betAmount;
         wins++;
+        gameOutcome.win = true;
     } else {
         resultMessage = `You guessed ${guess}, but the coin landed on ${flipResult}. Aw dangit! You lost $${betAmount}.`;
         document.getElementById("result").classList.add('lose-message');  
@@ -33,7 +38,9 @@ function flipCoin(guess) {
     document.getElementById("balance").innerHTML = balance.toFixed(2); 
     document.getElementById("wins").innerHTML = wins;
     document.getElementById("losses").innerHTML = losses;   
+    gameHistory.push(gameOutcome);
 }
+
 document.getElementById("balance").innerHTML = balance.toFixed(2);
 document.getElementById("wins").innerHTML = wins;
 document.getElementById("losses").innerHTML = losses;
